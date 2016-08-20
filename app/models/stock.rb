20348a167a95607f1e7f8df5a10c300ffee08753
Adam Gauvreau <adamgauvreau@hotmail.com>
2016-08-20 21:40:20 +0000

    Added stock ticker and stock traker

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ActiveRecord::Base
+   
+    #these 2 lines link the stock and user tables with the user_stock table (a stock can have multiple users and vice versa)
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
     
     def self.find_by_ticker(ticker_symbol)
         where(ticker: ticker_symbol).first
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  #these 2 lines link the stock and user tables with the user_stock table (a stock can have multiple users and vice versa)
+  has_many :user_stocks
+  has_many :stocks, through: :user_stocks
+  
+  #the 3 methods bellow will be used with if code in the lookup partial. We use them to show or hide a specific piece of information
+  #depending on the user stock portfolio (can he add a stock?, already 10 stock?, already tracking that stock?)
+  def can_add_stock?(ticker_symbol)
+    under_stock_limit? && !stock_already_added?(ticker_symbol)
+    
+  end
+  
+  def under_stock_limit?
+    (user_stocks.count < 10)
+  end
+  
+  def stock_already_added?(ticker_symbol)
+    stock = Stock.find_by_ticker(ticker_symbol)
+    return false unless stock
+    user_stocks.where(stock_id: stock.id).exists?
+  end
 end
