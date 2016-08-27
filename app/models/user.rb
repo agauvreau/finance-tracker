cebd4e8d9476f275ee4d3d39b454b455952e39d7
Adam Gauvreau <adamgauvreau@hotmail.com>
2016-08-27 03:37:36 +0000

    search friend function

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,23 +3,18 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  #these 2 lines link the stock and user tables with the user_stock table (a stock can have multiple users and vice versa)
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
   has_many :friendships
   has_many :friends, through: :friendships
   
-  #this method will return de first and last name of a user, if none are present it will return Anonymous
   def full_name
     return "#{first_name} #{last_name}".strip if (first_name || last_name)
     "Anonymous"
   end
   
-  #the 3 methods bellow will be used with if code in the lookup partial. We use them to show or hide a specific piece of information
-  #depending on the user stock portfolio (can he add a stock?, already 10 stock?, already tracking that stock?)
   def can_add_stock?(ticker_symbol)
     under_stock_limit? && !stock_already_added?(ticker_symbol)
-    
   end
   
   def under_stock_limit?
@@ -30,5 +25,37 @@ class User < ActiveRecord::Base
     stock = Stock.find_by_ticker(ticker_symbol)
     return false unless stock
     user_stocks.where(stock_id: stock.id).exists?
+  end
+  
+  def not_friends_with?(friend_id)
+    friendships.where(friend_id: friend_id).count < 1
+  end
+  
+  def except_current_user(users)
+    users.reject { |user| user.id == self.id }
+  end
+  
+  def self.search(param)
+    return User.none if param.blank?
+    
+    param.strip!
+    param.downcase!
+    (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+  end
+  
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+  
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+  
+  def self.email_matches(param)
+    matches('email', param)
+  end
+  
+  def self.matches(field_name, param)
+    where("lower(#{field_name}) like ?", "%#{param}%")
   end
 end
